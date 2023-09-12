dbc4b24c9089f85dae2947f515541dc4bb1dfddf
CookieXIV <nicolasottini97@gmail.com>
2023-09-12 16:57:56 +0200

    Inizia il progetto

---
+++ src/main/java/com/Github/ProgettoPersonale/ProgettoPersonaleApplication.java
@@ -0,0 +1,13 @@
+package com.Github.ProgettoPersonale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoPersonaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoPersonaleApplication.class, args);
+	}
+
+}
